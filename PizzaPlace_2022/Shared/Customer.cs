5aeb93d2a060d36fa49948286c44eb4461428820
Sang123a <19K4081024@hce.edu.vn>
2022-06-26 23:50:08 +0700

    first commit

---
+++ PizzaPlace_2022/Shared/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PizzaPlace_2022.Shared {
+    public class Customer {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Hãy điền họ tên")]
+        public string Name { get; set; } = default!;
+        [Required(ErrorMessage = "Hãy điền địa chỉ nhà")]
+        public string Street { get; set; } = default!;
+        [Required(ErrorMessage = "Hãy điền thành phố")]
+        public string City { get; set; } = default!;
+    }
+}
---
+++ PizzaPlace_2022/Shared/HardCodedMenuService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaPlace_2022.Shared {
+    public class HardCodedMenuService : IMenuService {
+        public ValueTask<Menu> GetMenu() => new ValueTask<Menu>(
+            new Menu {
+                Pizzas = new List<Pizza> {
+                    new Pizza(1, "Guitar Acoustic", 100.99M, Spiciness.GuitarClassic),
+                    new Pizza(2, "Guitar Classic", 110.99M, Spiciness.GuitarAucostic),
+                    new Pizza(3, "Ukulele", 75.99M, Spiciness.Ukulele)
+                }
+             });
+    }
+}
+
